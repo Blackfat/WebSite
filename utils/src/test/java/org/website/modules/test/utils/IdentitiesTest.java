d33f616ef2fad7e426b8a0b6ef9a682373bdfe89
Blackfat <15715556946@163.com>
2016-11-22 16:16:40 +0800

    基础工具类提交

---
+++ utils/src/test/java/org/website/modules/test/utils/IdentitiesTest.java
@@ -0,0 +1,15 @@
+package org.website.modules.test.utils;
+
+import org.junit.Test;
+import org.website.modules.utils.Identities;
+
+public class IdentitiesTest {
+
+    @Test
+    public void demo() {
+        System.out.println("uuid: " + Identities.uuid());
+        System.out.println("uuid2:" + Identities.uuid2());
+        System.out.println("randomLong:  " + Identities.randomLong());
+        System.out.println("randomBase62:" + Identities.randomBase62(7));
+    }
+}
